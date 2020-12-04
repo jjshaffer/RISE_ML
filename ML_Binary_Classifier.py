5ba7bb35a77ea95d974eaed246d2ff4af230173f
Joe Shaffer <jjshafferjr@gmail.com>
2020-12-04 12:08:19 -0600

    Update ML_Binary_Classifier.py

--- ML_Binary_Classifier.py
+++ ML_Binary_Classifier.py
@@ -8,10 +8,11 @@ March 6, 2020
 import torch
 import numpy as np
 import pandas as pd
+import sys
 from sklearn import preprocessing
 
-#Create neural network object as a pytorch Module that consists of a linear ANN with one hidden layer
 class BinaryClassifier(torch.nn.Module):
+    """Create neural network object as a pytorch Module that consists of a linear ANN with one hidden layer"""
     def __init__(self, D_in, H, D_out):
         super(BinaryClassifier, self).__init__()
         #Create first linear transform with input size D_in and output size H
@@ -35,7 +36,88 @@ class BinaryClassifier(torch.nn.Module):
         y_pred = self.layer_out(x)
         
         return y_pred
+    
 
+def generate_balanced_datasets(data, train_a, train_b, test_a, test_b, verification_a, verification_b, var):
+    """Function for generating datasets with an equal number of participants in each group by resampling """
+    
+    #Factorize the category in order to account for text
+    temp = pd.factorize(data[var])
+    
+     
+    #Replace column with numeric category label  
+    data[var] = temp[0]
+
+    #Create separate lists for each variable category
+    a_data = data.loc[data[var]==1]
+    b_data = data.loc[data[var]!=1]
+    
+    #Shuffle lists to randomize them
+    shuffled_a = a_data.sample(frac=1).reset_index(drop=True)
+    shuffled_b = b_data.sample(frac=1).reset_index(drop=True)
+    
+    #Extract random testing Sets from data
+    if len(shuffled_a) > test_a:
+        test_a_data = shuffled_a.loc[range(0,test_a), :]
+    else:
+        print('Error: Insufficient Participants for Test Set')
+    
+    
+    if len(shuffled_b) > test_b:
+        test_b_data = shuffled_b.loc[range(0,test_b), :]
+    else:
+        print('Error: Insufficient Participants for Test Set')
+        
+     #Extract verification sets from remaining data
+     
+    if (len(shuffled_a)-test_a) >= verification_a:
+        verification_a_data = shuffled_a.loc[range(test_a, test_a + verification_a), :]
+        
+    else: 
+        print('Error: Insufficient Participants for Verification Set')
+        
+    if (len(shuffled_b)-test_b) >= verification_b:
+        verification_b_data = shuffled_b.loc[range(test_b, test_b + verification_b), :]
+        
+    else: 
+        print('Error: Insufficient Participants for Verification Set')   
+        
+    #Extract training sets from remaining data
+    if (len(shuffled_a) - test_a - verification_a) >= train_a:
+        train_a_data = shuffled_a.loc[range(test_a + verification_a, test_a + verification_a + train_a), :]
+        
+    else:
+        remaining_a = shuffled_a.loc[range(test_a + verification_a, len(shuffled_a)), :]
+        part1 = remaining_a
+        part2 = remaining_a.sample(n=(train_a-len(remaining_a)), replace=True).reset_index(drop=True)
+        
+        train_a_data = pd.concat([part1, part2], axis=0)
+        train_a_data = train_a_data.sample(frac=1).reset_index(drop=True)
+    
+    if (len(shuffled_b) - test_b - verification_b) >= train_b:
+        train_b_data = shuffled_b.loc[range(test_b + verification_b, test_b + verification_b + train_b), :]
+        
+    else:
+        remaining_b = shuffled_b.loc[range(test_b + verification_b, len(shuffled_b)), :]
+        
+        part1 = remaining_b
+        part2 = remaining_b.sample(n=(train_b-len(remaining_b)), replace=True).reset_index(drop=True)
+        
+        train_b_data = pd.concat([part1, part2], axis=0)
+        train_b_data = train_b_data.sample(frac=1).reset_index(drop=True)
+
+    
+    test_data = pd.concat([test_a_data, test_b_data], axis=0)
+    test_data = test_data.sample(frac=1).reset_index(drop=True)
+    
+    train_data = pd.concat([train_a_data, train_b_data], axis=0)
+    train_data = train_data.sample(frac=1).reset_index(drop=True)
+    
+    verification_data = pd.concat([verification_a_data, verification_b_data], axis = 0)
+    verification_data = verification_data.sample(frac=1).reset_index(drop=True)
+    
+    return train_data, test_data, verification_data
+    
 def generate_random_datasets(data, n, m, var):
     """ This function generates a random sub-sample with n samples from category 0 and m samples from category 1 of a binary classifier, var"""
     #Shuffle the dataset rows, reset the index
@@ -124,11 +206,16 @@ def train_classifier_model(model, input_tensor, ans_tensor, epochs):
     #Set model to training mode
     model.train()
     
+    #print(str(input_tensor.shape))
+    #print(str(ans_tensor.shape))
+    
     #Loop through training cycles
     for t in range(epochs):
         #Get prediction from neural network
         ans_pred = model(input_tensor.float())
                 
+        ans_pred = torch.Tensor(ans_pred)
+        
         #Calculate error & print result so we can see improvement
         loss = criterion(ans_pred, ans_tensor.float())
             
@@ -136,10 +223,31 @@ def train_classifier_model(model, input_tensor, ans_tensor, epochs):
         optimizer.zero_grad()
         #Backpropagate to train network
         loss.backward()
+        ##torch.nn.utils.clip_grad_norm_(model.parameters(), 5)
         optimizer.step()
-        
+    
     return model, ans_pred, loss
 
+def load_input_data(data_file, test_var_label, data_start_column_label, data_end_column_label):
+    """ Loads input and answer data from saved .csv file and generates tensors for input into model """
+    
+    all_data = pd.read_csv(data_file)
+    
+    #Get column with variable of interest as answer and convert to tensor
+    ans = pd.factorize(all_data[test_var_label])
+    ans_tensor = torch.from_numpy(ans[0]).unsqueeze(1)
+    
+    
+    #Get range of imaging data from input file
+    imaging_variables = all_data.loc[:, data_start_column_label:data_end_column_label]
+    
+    #Normalize each column of imaging data and convert to tensor
+    scaler = preprocessing.StandardScaler()
+    imaging_data = pd.DataFrame(scaler.fit_transform(imaging_variables.values))
+    input_tensor = torch.tensor(imaging_data.values)
+    
+    return input_tensor, ans_tensor
+    
 def load_model(filename, D_in, H, D_out):
     """ Function for loading a saved model file and rebuilding it into a pyTorch model"""
     #Initialize empty NeuralNet object
@@ -154,26 +262,74 @@ def load_model(filename, D_in, H, D_out):
     model.eval()
     return model
 
+def select_key_features(data_file, sorting_output, outfilename):
+    """ Function for taking the sorted output of the find_Consensus_Results function and splitting those results into a new .csv containing only better than average features """
+    
+    data_df = pd.read_csv(data_file)
+    
+    t = sorting_output.sum() > 0
+    
+    name_list = []
+    for col in t.index:
+        if not t.loc[col]:
+            name_list.append(col)
+        
+    less_data = data_df.drop(name_list, axis=1)
+    less_data.to_csv(outfilename, index=False)
+    
+    return less_data
 
+def generate_BinaryClassifier_model_BASH_wrapper():
+    #print(str(len(sys.argv)))
+        
+    for i in range(0,len(sys.argv)):
+        print("arg: ", str(i), ": ", str(sys.argv[i]), ':', str(type(sys.argv[i])))
+    
+    data_file=sys.argv[1]
+    train_group_1_num=int(sys.argv[2])
+    train_group_2_num=int(sys.argv[3])
+    test_group_1_num=int(sys.argv[4])
+    test_group_2_num=int(sys.argv[5])
+    verification_group_1_num=int(sys.argv[6])
+    verification_group_2_num=int(sys.argv[7])
+    test_var_label=sys.argv[8]
+    data_start_column_label=sys.argv[9]
+    data_end_column_label=sys.argv[10]
+    training_cycles=int(sys.argv[11])
+    outprefix=sys.argv[12]
+    iteration=int(sys.argv[13])
+    
+    
+    
+    model, pred, h, fp, fn = generate_BinaryClassifier_model(data_file, train_group_1_num, train_group_2_num, test_group_1_num, test_group_2_num, verification_group_1_num, verification_group_2_num, test_var_label, data_start_column_label, data_end_column_label, training_cycles, outprefix, iteration)
 
-def generate_BinaryClassifier_model(data_file, train_group_1_num, train_group_2_num, test_var_label, data_start_column_label, data_end_column_label, training_cycles, outprefix):
+    #model, pred, h, fp, fn = generate_BinaryClassifier_model(str(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]), int(sys.argv[4]), int(sys.argv[5]), int(sys.argv[6]), int(sys.argv[7]), str(sys.argv[8]), str(sys.argv[9]), str(sys.argv[10]), int(sys.argv[11]), str(sys.argv[12]), int(sys.argv[13]))
+    
+    return 1, 1, 1, 1, 1
+    #return model, pred, h, fp, fn
+
+def generate_BinaryClassifier_model(data_file, train_group_1_num, train_group_2_num, test_group_1_num, test_group_2_num, verification_group_1_num, verification_group_2_num, test_var_label, data_start_column_label, data_end_column_label, training_cycles, outprefix, iteration):
+    """This function creates a binary classifier model by generating a random training, verification, and test set and then training/testing a binary classifier model """
+    
+    print('RUNNING')
     
     #Read Data File
     all_data = pd.read_csv(data_file)
     
     #Generate random training and test datasets    
-    train_data, test_data = generate_random_datasets(all_data, train_group_1_num, train_group_2_num, test_var_label)
+    train_data, test_data, verification_data = generate_balanced_datasets(all_data, train_group_1_num, train_group_2_num, test_group_1_num, test_group_2_num, verification_group_1_num, verification_group_2_num, test_var_label)
 
     #Convert categorical labels for variable of interest to numeric values. For group, 0=Case, 1 = Control
     train_group = pd.factorize(train_data[test_var_label])
     test_group = pd.factorize(test_data[test_var_label])
-   
+    
     #Create tensors from variables of interest
     train_ans_tensor = torch.from_numpy(train_group[0]).unsqueeze(1)
     test_ans_tensor = torch.from_numpy(test_group[0]).unsqueeze(1)
-
+  
+    print(data_start_column_label, ':', data_end_column_label)
     #Get imaging variable portion of the training and test data to input to NN
-    train_imaging_variables=train_data.loc[:, data_start_column_label:data_end_column_label]
+    train_imaging_variables= train_data.loc[:, data_start_column_label:data_end_column_label]
     test_imaging_variables = test_data.loc[:, data_start_column_label:data_end_column_label]
 
     #Normalize each column of training and test data
@@ -195,6 +351,7 @@ def generate_BinaryClassifier_model(data_file, train_group_1_num, train_group_2_
     #Set size of hidden layer based on size of inputs and outputs
     H = int(np.floor((D_in + D_out)/2))
 
+    #device = torch.device("cpu")
     #Define Neural Network based on input size
     model = BinaryClassifier(D_in, H, D_out)
     
@@ -210,16 +367,49 @@ def generate_BinaryClassifier_model(data_file, train_group_1_num, train_group_2_
     #Test accuracy of model
     test_ans = test_ans_tensor.detach().numpy() 
     binary_test_pred = torch.round(torch.sigmoid(test_ans_pred)).detach().numpy()
+    
     h,fp,fn = get_accuracy_metrics(binary_test_pred,test_ans)
 
     #Store model, test_data
-    model_filename = outprefix + '_acc_' + str(float(np.mean(h))) + '_model.pt'
+    model_filename = outprefix + '_acc_' + str(round(float(np.mean(h)), 3)) + '_In_' + str(D_in) + '_H_'+ str(H) + '_Out_' + str(D_out) + '_' + str(iteration) + '_model.pt'
+    print(model_filename)
     torch.save(model.state_dict(), model_filename)
-    data_filename = outprefix +  '_train_data.csv'
+    data_filename = outprefix + '_acc_' + str(round(float(np.mean(h)), 3)) + '_In_' + str(D_in) + '_H_'+ str(H) + '_Out_' + str(D_out) + '_' + str(iteration) + '_train_data.csv'
     train_data.to_csv(data_filename, index = False)
-    data_filename = outprefix +  '_test_data.csv'
+    data_filename = outprefix + '_acc_' + str(round(float(np.mean(h)), 3)) + '_In_' + str(D_in) + '_H_'+ str(H) + '_Out_' + str(D_out) + '_' + str(iteration)+ '_test_data.csv'
     test_data.to_csv(data_filename, index = False)
 
+    #If a verification set is generated, also test verification data set
+    if (len(verification_data) > 0):
+        
+        #Select predicted category and create tensor for expected values
+        verification_group = pd.factorize(verification_data[test_var_label])
+        verification_ans_tensor = torch.from_numpy(verification_group[0]).unsqueeze(1)
+        
+        #Select imaging data columns
+        verification_imaging_variables = verification_data.loc[:, data_start_column_label:data_end_column_label]
+        
+        #Normalize imaging data
+        verification_imaging_data = pd.DataFrame(scaler.fit_transform(verification_imaging_variables.values))
+        
+        #Convert to tensor
+        verification_input_tensor = torch.tensor(verification_imaging_data.values)
+
+        #Test Model Prediction on test set
+        model.eval()
+        with torch.no_grad():
+            #Get prediction from neural network
+            verification_ans_pred = model(verification_input_tensor.float())
+
+        #Test accuracy of model
+        verification_ans = verification_ans_tensor.detach().numpy() 
+        binary_verification_pred = torch.round(torch.sigmoid(verification_ans_pred)).detach().numpy()
+        
+        verification_h,verification_fp,verification_fn = get_accuracy_metrics(binary_verification_pred,verification_ans)
+
+        data_filename = outprefix + '_acc_' + str(round(float(np.mean(h)), 3)) + '_ver-acc_' + str(round(float(np.mean(verification_h)), 3)) + '_In_' + str(D_in) + '_H_'+ str(H) + '_Out_' + str(D_out) + '_' + str(iteration)+ '_verification_data.csv'
+        test_data.to_csv(data_filename, index = False)
+    
     return model, binary_test_pred, h, fp, fn
 
 
